d29e524002c31305f189982019057a7b0290f877
Oxiplegatz <104036115+Oxiplegatz@users.noreply.github.com>
2022-11-15 19:46:02 +0300

    Revert "Merge branch 'develop' into featute/review"
    This reverts commit 6b090a12027eaab37c54abc9231c5d8dc47e9589, reversing
    changes made to 68cbc0567154cd8a0b4a6e9fcf00de5f33993155.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,58 +1,9 @@
-# from django.contrib.auth import get_user_model
-from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
 
 MIN_VALUE_SCORE = 1
 MAX_VALUE_SCORE = 10
-# User = get_user_model()
-
-
-class Genre(models.Model):
-    """Модель, хранящая данные о жанрах."""
-    name = models.CharField('Название жанра', max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    """Модель, хранящая данные о категориях."""
-    name = models.CharField('Название категории', max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Title(models.Model):
-    """Модель, хранящая данные о произведениях."""
-    name = models.CharField('Название произведения', max_length=256)
-    year = models.DateField(
-        'Год выхода', input_formats=['%Y', 'iso-8601'], format='%Y'
-    )
-    rating = models.IntegerField(
-        'Рейтинг', default=None, required=False, max_value=10, min_value=1
-    )
-    description = models.TextField('Описание произведения', required=False)
-    genre = models.ManyToManyField(
-        Genre, through='GenreTitle', blank=True, null=True
-    )
-    category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, related_name='titles'
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class GenreTitle(models.Model):
-    """Модель, реализующая связь многие-ко-многим Произведений и Жанров."""
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.title} {self.genre}'
 
 
 class Review(models.Model):
