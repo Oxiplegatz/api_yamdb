da4bc812203f880bda19faf42902fa922e123a20
Alexei Kadaner <minushuman@yandex.ru>
2022-11-15 21:27:49 +0300

    Исправляем хаос с пул реквестами.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,9 @@
 # from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
+MIN_VALUE_SCORE = 1
+MAX_VALUE_SCORE = 10
 # User = get_user_model()
 
 
@@ -50,3 +53,66 @@ class GenreTitle(models.Model):
 
     def __str__(self):
         return f'{self.title} {self.genre}'
+
+
+class Review(models.Model):
+    """Модель, хранящая отзывы о произведениях."""
+    title = models.ForeignKey(
+        Title,
+        related_name='reviews',
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField(
+        'Текст отзывa',
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='reviews',
+        on_delete=models.CASCADE,
+    )
+    score = models.IntegerField(
+        'Оценка произведения',
+        validators=[
+            MinValueValidator(MIN_VALUE_SCORE),
+            MaxValueValidator(MAX_VALUE_SCORE)
+        ],
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:50]
+
+
+class Comment(models.Model):
+    """Модель, хранящая комментарии к отзывам о произведениях."""
+    review = models.ForeignKey(
+        Review,
+        related_name='comments',
+        verbose_name='Отзыв',
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField(
+        'Текст комментария к отзыву',
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='comments',
+        on_delete=models.CASCADE,
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации комментария к отзыву',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:50]
