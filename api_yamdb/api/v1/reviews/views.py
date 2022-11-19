4c8445c8f4fa4013a044d21b9ef79cfa3ee02dea
Анастасия Закирова <aazakirova54@yandex.ru>
2022-11-19 12:08:37 +0700

    Пермишены

--- api_yamdb/api/v1/reviews/views.py
+++ api_yamdb/api/v1/reviews/views.py
@@ -11,7 +11,7 @@ from reviews.models import Category, Genre, Title, Review
 from api.v1.reviews.serializers import (CategorySerializer, CommentSerializer,
                                         GenreSerializer, TitleSerializer,
                                         TitlePostSerializer, ReviewSerializer)
-from api.v1.users.permissions import IsAdmin, IsOwner
+from api.v1.users.permissions import IsAdmin, IsAuthorAdminModeratorOrReadOnly, IsOwner
 
 
 class TitleFilter(django_filters.FilterSet):
@@ -104,7 +104,7 @@ class TitleViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     """Вьюсет для модели Review."""
     serializer_class = ReviewSerializer
-    # permission_classes = []
+    permission_classes = (IsAuthorAdminModeratorOrReadOnly, )
 
     def perform_create(self, serializer):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
@@ -118,7 +118,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     """Вьюсет для модели Comment."""
     serializer_class = CommentSerializer
-    # permission_classes = []
+    permission_classes = (IsAuthorAdminModeratorOrReadOnly, )
 
     def perform_create(self, serializer):
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
